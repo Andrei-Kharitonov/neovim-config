a41fec51a81e7b85c473d3bcf1c84388e61dd603
Andrei-Kharitonov <89729599+Andrei-Kharitonov@users.noreply.github.com>
2025-11-26 23:05:25 +0400

    added telescope file browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
--- Searcher
 return {
   {
+    -- Searcher
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     dependencies = {
@@ -8,12 +8,18 @@ return {
     },
     event = "VeryLazy",
     config = function()
+      local fb_actions = require "telescope".extensions.file_browser.actions
       require("telescope").setup({
         defaults = {
           layout_strategy = "horizontal",
           sorting_strategy = "ascending",
-          preview = {
-            hide_on_startup = true,
+          layout_config = {
+            horizontal = {
+              width = 0.8,
+              height = 0.8,
+              preview_cutoff = 60,
+              prompt_position = "top",
+            },
           },
           mappings = {
             i = {
@@ -33,17 +39,31 @@ return {
               ["q"] = "close",
             },
           },
-          layout_config = {
-            horizontal = {
-              width = 0.6,
-              height = 0.7,
-              preview_cutoff = 60,
-              prompt_position = "top",
+        },
+        extensions = {
+          file_browser = {
+            display_stat = false,
+            dir_icon = "",
+            grouped = true,
+            dir_icon_hl = "Directory",
+            hide_parent_dir = true,
+            mappings = {
+              ["n"] = {
+                ["<BS>"] = fb_actions.goto_parent_dir,
+                ["H"] = fb_actions.toggle_hidden,
+                ["."] = fb_actions.toggle_hidden,
+                ["h"] = false,
+                ["g"] = false,
+              },
+              ["i"] = {
+                ["<C-.>"] = fb_actions.toggle_hidden,
+              },
             },
           },
         },
       })
       require("telescope").load_extension("notify")
+      require("telescope").load_extension("file_browser")
     end,
   },
   {
@@ -61,4 +81,12 @@ return {
       require("telescope").load_extension("ui-select")
     end,
   },
+
+  {
+    -- Telescope file browser
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    }
+  }
 }
